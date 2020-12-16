2235ba55fbe25a80a8031c7057e455617a337d00
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-12-16 18:39:59 +0530

    [Saiprasad]Added: methods to load the csv files and sorted the batsman data based on top batting average and written that sorted data into json file

---
+++ src/main/java/ipl/analyser/IPLAnalyser.java
@@ -0,0 +1,76 @@
+package ipl.analyser;
+
+import com.google.gson.Gson;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import jar.builder.*;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class IPLAnalyser {
+
+    public static final String TOP_BATTING_AVG_FILE = "C:\\Users\\saiprasad\\IdeaProjects\\IPLAnaluser\\IPLAnaluser\\src\\test\\resources\\TopBattingAvg.json";
+    public static List<IPLBatsmanStats> iplBatsmanStats;
+    public static List<IPLBowlerStats> iplBowlerStats;
+
+    public int loadIplBatsmanStat(String csvFilePath) throws IPLAnalyserException {
+        try(Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+        CSVReader csvReader = new CSVReader(reader);){
+
+            iplBatsmanStats = getCSVFileList(reader,IPLBatsmanStats.class);
+            return  iplBatsmanStats.size();
+        } catch (IOException| RuntimeException e ) {
+            throw new IPLAnalyserException(e.getMessage(),IPLAnalyserException.ExceptionType.IPL_FILE_PROBLEM);
+        }
+
+    }
+
+    public int loadIplBowlerStat(String csvFilePath) throws IPLAnalyserException {
+        try(Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+            CSVReader csvReader = new CSVReader(reader);){
+
+            iplBowlerStats = getCSVFileList(reader,IPLBowlerStats.class);
+            return  iplBowlerStats.size();
+        } catch (IOException| RuntimeException e ) {
+            throw new IPLAnalyserException(e.getMessage(),IPLAnalyserException.ExceptionType.IPL_FILE_PROBLEM);
+        }
+
+    }
+
+    public <E>List getCSVFileList(Reader reader, Class csvClass)  {
+
+        CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+        csvToBeanBuilder.withType(csvClass);
+        csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+        CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+        return csvToBean.parse();
+    }
+
+    public void sortingBasedOnBattingAverage() throws IOException {
+        Comparator com = new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                IPLBatsmanStats b1 = (IPLBatsmanStats)o1;
+                IPLBatsmanStats b2 = (IPLBatsmanStats)o2;
+                if(b1.getAverage()>b2.getAverage())
+                    return -3;
+                else
+                    return 3;
+            }
+        };
+        Collections.sort(iplBatsmanStats,com);
+        Gson gson = new Gson();
+        String json =gson.toJson(iplBatsmanStats);
+        FileWriter fileWriter = new FileWriter(TOP_BATTING_AVG_FILE);
+        fileWriter.write(json);
+        fileWriter.close();
+    }
+}
---
+++ src/main/java/ipl/analyser/IPLAnalyserException.java
@@ -0,0 +1,17 @@
+package ipl.analyser;
+
+public class IPLAnalyserException extends Exception {
+
+        enum ExceptionType{
+            IPL_FILE_PROBLEM, IPL_HEADER_DELIMETER_PROBLEM, FILE_OR_HEADER_PROBLEM, NO_SUCH_DATA;
+        }
+
+        ExceptionType type;
+
+        public IPLAnalyserException(String message, ExceptionType type) {
+            super(message);
+            this.type = type;
+        }
+}
+
+
